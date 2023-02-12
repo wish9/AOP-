4238fdd41cae3862e6cbf1abdf956c8a1101d2a5
wish9 <wjwee9@gmail.com>
2023-02-12 21:11:10 +0900

    AOP연습 - 스프링 AOP 구현

---
+++ src/main/java/start/aop/order/aop/Aspect1.java
@@ -0,0 +1,16 @@
+package start.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class Aspect1 {
+    @Around("execution(* start.aop.order..*(..))") // 포인트컷 표현식 직접 넣은 case
+    public Object logging(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("log -> {}", joinPoint.getSignature());
+        return joinPoint.proceed();
+    }
+}
